e3ea8952c053dbd9c41dd9cadd6ce6d2f877e9cf
Davis Byamugisha <davisb256@gmail.com>
2020-08-30 15:55:53 +0300

    Added the index file

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Silence is golden
+
+
+$var = 'This is just a sentence';
+
+
+$var2 = 'This is another sentence';
+
+
+$var3 = 'We are going to use these two sentences to test edited lines of code';
+
+
+
